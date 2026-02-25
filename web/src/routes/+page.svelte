e809066998728b8b6706deba58c71f8c54174dca
wighawag <wighawag@gmail.com>
2026-02-25 18:17:02 +0000

    empty

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,52 +1,24 @@
 <script lang="ts">
-	import {Button, buttonVariants} from '$lib/shadcn/ui/button/index.js';
-	import * as Dialog from '$lib/shadcn/ui/dialog/index.js';
-	import {Input} from '$lib/shadcn/ui/input/index.js';
-	import {Label} from '$lib/shadcn/ui/label/index.js';
-
 	import DefaultHead from './metadata/DefaultHead.svelte';
-
-	let dialogOpen = $state(false);
+	import * as Empty from '$lib/shadcn/ui/empty/index.js';
+	import {BellIcon, LogInIcon} from '@lucide/svelte';
+	import {Button} from '$lib/shadcn/ui/button';
 </script>
 
 <DefaultHead title={'Template'} />
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-	documentation
-</p>
-
-<Button onclick={() => (dialogOpen = true)}>open</Button>
-
-<Dialog.Root bind:open={dialogOpen}>
-	<form>
-		<Dialog.Content class="sm:max-w-[425px]">
-			<Dialog.Header>
-				<Dialog.Title>Edit profile</Dialog.Title>
-				<Dialog.Description>
-					Make changes to your profile here. Click save when you&apos;re done.
-				</Dialog.Description>
-			</Dialog.Header>
-			<div class="grid gap-4">
-				<div class="grid gap-3">
-					<Label for="name-1">Name</Label>
-					<Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-				</div>
-				<div class="grid gap-3">
-					<Label for="username-1">Username</Label>
-					<Input id="username-1" name="username" defaultValue="@peduarte" />
-				</div>
-			</div>
-			<Dialog.Footer>
-				<Dialog.Close
-					type="button"
-					class={buttonVariants({variant: 'outline'})}
-				>
-					Cancel
-				</Dialog.Close>
-				<Button type="submit">Save changes</Button>
-			</Dialog.Footer>
-		</Dialog.Content>
-	</form>
-</Dialog.Root>
+<Empty.Root class="h-full bg-linear-to-b from-muted/50 from-30% to-background">
+	<Empty.Header>
+		<Empty.Media variant="icon">
+			<BellIcon />
+		</Empty.Media>
+		<Empty.Title>Nothing to show</Empty.Title>
+		<Empty.Description>There is currently nothing to show</Empty.Description>
+	</Empty.Header>
+	<Empty.Content>
+		<Button variant="outline" size="sm">
+			<LogInIcon />
+			Sign-In
+		</Button>
+	</Empty.Content>
+</Empty.Root>
