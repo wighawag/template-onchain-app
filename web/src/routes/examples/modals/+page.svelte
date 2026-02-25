327e56e5659ad6d92a013b9559f7b7283aadd70f
wighawag <wighawag@gmail.com>
2026-02-25 14:37:30 +0000

    Modal

---
+++ web/src/routes/examples/modals/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import {Button} from '$lib/shadcn/ui/button/index.js';
+	import * as Modal from '$lib/core/ui/modal/index.js';
+
+	let dialogOpen = $state(false);
+
+	let button: HTMLButtonElement | null = $state(null);
+</script>
+
+<Button onclick={() => (dialogOpen = true)}>open</Button>
+
+<Modal.Root
+	openWhen={dialogOpen}
+	onCancel={() => (dialogOpen = false)}
+	elementToFocus={button}
+>
+	<Modal.Header>
+		<Modal.Title>Hello world</Modal.Title>
+		<Modal.Description>sdsadsa</Modal.Description>
+	</Modal.Header>
+	Enter
+	<Modal.Footer
+		><Button>hi</Button>
+		<Button
+			variant="outline"
+			onclick={() => console.log(`clicked`)}
+			bind:ref={button}>hi</Button
+		></Modal.Footer
+	>
+</Modal.Root>
