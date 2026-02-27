5de1163a78c2b1babb53143ab5cc2dba221f0450
wighawag <wighawag@gmail.com>
2026-02-27 12:03:58 +0000

    top o + padding for navbar

--- web/src/lib/core/ui/navbar/navbar.svelte
+++ web/src/lib/core/ui/navbar/navbar.svelte
@@ -18,8 +18,9 @@
 	}
 </script>
 
+<!--nabvar have some top padding to match the scrollbar gutter on the right, see app.css-->
 <nav
-	class="sticky top-2 left-0 z-50 flex h-12 w-full items-center justify-between bg-background shadow-md"
+	class="sticky top-0 left-0 z-50 flex h-12 w-full items-center justify-between bg-background pt-4 shadow-md"
 >
 	<div class="m-1 flex h-full items-center space-x-2">
 		<span class="text-lg font-bold">{name}</span> <a href={url('/')}>home</a>
